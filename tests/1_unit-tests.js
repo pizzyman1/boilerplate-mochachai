ef3020ed257b99d29107f1925625998c229153bd
pizzyman1 <79599044+pizzyman1@users.noreply.github.com>
2023-11-04 19:55:44 +0000

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,8 +5,8 @@ suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
     // #1
     test('#isNull, #isNotNull', function () {
-      assert.fail(null, 'This is an optional error description - e.g. null is null');
-      assert.fail(1, '1 is not null');
+      assert.null(null, 'This is an optional error description - e.g. null is null');
+      assert.is not null(1, '1 is not null');
     });
     // #2
     test('#isDefined, #isUndefined', function () {
